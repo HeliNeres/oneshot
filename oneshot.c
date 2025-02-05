dcd2e558d2e95397c8c2b8c8b3b45ab4a29cf9d5
Heli N. S. <helineres@hotmail.com>
2025-02-04 23:54:57 -0300

    corrigidos erros de código

--- oneshot.c
+++ oneshot.c
@@ -1,5 +1,7 @@
+#include <stdio.h>
 #include "pico/stdlib.h"
 #include "pico/time.h"
+#include "pico/bootrom.h"
 
 // Variáveis dos LEDs
 #define LED_VERDE 11
@@ -8,7 +10,7 @@
 
 //pino do botão
 const uint BUTTON_A = 5;
-const uint BUTTON_B = 5;
+const uint BUTTON_B = 6;
 
 bool botao_on = false;
 bool led_active = false;
@@ -17,21 +19,22 @@ uint last_time = 0;
 bool debounce = true;
 
 //cabeçalhos da funções
-void inicializar_leds();
+void inicializar();
 int64_t turn_off_green(alarm_id_t id, void *user_data);
 int64_t turn_off_blue(alarm_id_t id, void *user_data);
 int64_t turn_off_red(alarm_id_t id, void *user_data);
 static void botao_boot(uint gpio, uint32_t events);
-static void botao(uint gpio, uint32_t events);
 
 int main() {
     stdio_init_all();
+    inicializar();
 
-    gpio_set_irq_enabled_with_callback(BUTTON_B, GPIO_IRQ_EDGE_FALL, 1, & botao_boot);
+    gpio_set_irq_enabled_with_callback(BUTTON_B, GPIO_IRQ_EDGE_FALL, 1, &botao_boot);
 
     while (true) {
         if (gpio_get(BUTTON_A) == 0 && !led_active) {
             sleep_ms(50); //debounce
+            //printf("Botão\n");
 
             if (gpio_get(BUTTON_A) == 0) {
                 gpio_put(LED_VERDE, 1);
@@ -52,7 +55,7 @@ int main() {
     return 0;
 }
 
-void inicializar_leds(){
+void inicializar(){
 	gpio_init(LED_VERDE);
 	gpio_init(LED_AZUL);
 	gpio_init(LED_VERMELHO);
@@ -72,21 +75,21 @@ void inicializar_leds(){
 
 // função de callback para desligar o LED verde após o tempo programado
 int64_t turn_off_green(alarm_id_t id, void *user_data) {
-    gpio_put(LED_VERDE, false);
+    gpio_put(LED_VERDE, 0);
 
     return 0;
 }
 
 // função de callback para desligar o LED azul após o tempo programado
 int64_t turn_off_blue(alarm_id_t id, void *user_data) {
-    gpio_put(LED_AZUL, false);
+    gpio_put(LED_AZUL, 0);
 
     return 0;
 }
 
 // função de callback para desligar o LED vermelho após o tempo programado
 int64_t turn_off_red(alarm_id_t id, void *user_data) {
-    gpio_put(LED_VERMELHO, false);
+    gpio_put(LED_VERMELHO, 0);
 
     led_active = false;
 
@@ -95,7 +98,7 @@ int64_t turn_off_red(alarm_id_t id, void *user_data) {
 
 //rotina da interrupção para boot
 static void botao_boot(uint gpio, uint32_t events){
-    printf("Interrupção ocorreu no pino %d, no evento %d\n", gpio, events);
+    printf("Interrupção ocorreu no pino %u, no evento %u\n", gpio, events);
     printf("HABILITANDO O MODO GRAVAÇÃO");
 	reset_usb_boot(0,0); //habilita o modo de gravação do microcontrolador
 }
